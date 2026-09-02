7e443976fbe1ed2a5b6136cef075439e1b6734dd
VPS APEX GLASS <vps@apex.glass>
2026-09-02 15:07:38 +0000

    Auto-sync 2026-09-02 15:07

--- app/modulos/media_manager.php
+++ app/modulos/media_manager.php
@@ -72,7 +72,7 @@ header('Content-Type: text/html; charset=utf-8');
 <script>
 var ModMedia = (function () {
   var API = '../api/media_manager.php';
-  var CHUNK = 6 * 1024 * 1024; // margen bajo post_max_size (8M) por overhead de multipart/form-data
+  var CHUNK = 1.5 * 1024 * 1024; // margen bajo upload_max_filesize (2M, más estricto que post_max_size 8M) por overhead de multipart/form-data
   var path = '';
 
   function esc(s) {
